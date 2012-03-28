4a1ac4f5b23abedf3ca8e34a9becb1e370ad7a6a
Andreas Gruenenfelder <gruenean@students.zhaw.ch>
2012-03-29 00:19:28 +0200

    getRandomNeighbour added

--- src/labyrinth/Maze.java
+++ src/labyrinth/Maze.java
@@ -1,5 +1,6 @@
 package labyrinth;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -28,9 +29,12 @@ public class Maze {
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
 				Cell newCell = new Cell();
+				
+				//TODO Testing only - remove
+				newCell.setValue(""+row+col);
 				map[row][col] = newCell;
 				relations.put(newCell, newCell);
-				positions.put(new int[]{1,1}, newCell);
+				positions.put(new int[]{row,col}, newCell);
 			}
 		}
 	}
@@ -64,8 +68,8 @@ public class Maze {
 	 * @param cell	 
 	 * @return		position of the given cell
 	 */
-	//TODO: set method to private - currently used for testing 
-	public int[] getPositionOfCell(Cell cell) {
+	//TODO: 'return new int[0]' ugly
+	private int[] getPositionOfCell(Cell cell) {
 		for (Map.Entry<int[], Cell> entry: positions.entrySet()) {
 			if (entry.getValue().equals(cell)) {
 				return entry.getKey();
@@ -74,11 +78,36 @@ public class Maze {
 		return new int[0];
 	}
 	
-//	public Cell getRandomNeighbour(Cell cell) {
-//		int[] position = getPositionOfCell(cell);
-//		ArrayList<int[]> neighbours= new ArrayList<int[]>();
-//		neighbours.add(arg0)
-//	}
+	//TODO: 'return new Cell()' ugly
+	private Cell getCellOnPosition(int[] pos) {
+		for (Map.Entry<int[], Cell> entry: positions.entrySet()) {
+			if (entry.getKey()[0] == pos[0] && entry.getKey()[1] == pos[1]) {
+				return entry.getValue();
+			}
+		}
+		return new Cell();
+	}
+	
+	/**
+	 * @param 	
+	 * @return 	Returns a randomly chosen neighbour of input cell
+	 */
+	public Cell getRandomNeighbour(Cell cell) {
+		int[] currentPos = getPositionOfCell(cell);
+		
+		ArrayList<int[]> neighbourPositions= new ArrayList<int[]>();
+		neighbourPositions.add(new int[]{currentPos[0],currentPos[1]-1}); // neighour left
+		neighbourPositions.add(new int[]{currentPos[0],currentPos[1]+1}); // neighbour right
+		neighbourPositions.add(new int[]{currentPos[0]-1,currentPos[1]}); // neighbour top
+		neighbourPositions.add(new int[]{currentPos[0]+1,currentPos[1]}); // neighour bottom
+		
+		getCellOnPosition(neighbourPositions.get(0)).setValue("LL");
+		getCellOnPosition(neighbourPositions.get(1)).setValue("RR");
+		getCellOnPosition(neighbourPositions.get(2)).setValue("TT");
+		getCellOnPosition(neighbourPositions.get(3)).setValue("BB");
+		
+		return getCellOnPosition(neighbourPositions.get(new Random().nextInt(neighbourPositions.size())));
+	}
 	
 //	public void create_One_Solution(){
 //		
