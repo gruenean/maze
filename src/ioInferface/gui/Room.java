334415c4f5ddc825672786412b16cdbad9c2774b
Andreas Gruenenfelder <gruenean@students.zhaw.ch>
2012-05-29 17:22:30 +0200

    Room and Wall added

---
+++ src/ioInferface/gui/Room.java
@@ -0,0 +1,11 @@
+package ioInferface.gui;
+
+import ch.aplu.jgamegrid.Actor;
+
+public class Room extends Actor {
+
+	public Room() {
+		super(true, "sprites/white_background.png");
+		this.setCollisionImage();
+	}
+}
---
+++ src/ioInferface/gui/Wall.java
@@ -0,0 +1,19 @@
+package ioInferface.gui;
+
+import ch.aplu.jgamegrid.Actor;
+
+/**
+ * This Cell class is a faked cell class. The used Frameword JGramgrid gives the
+ * possibilities of an actor in a GUI. So the cell of the maze is an actor which
+ * just use a black background.
+ * 
+ * @author micha
+ * 
+ */
+class Wall extends Actor {
+
+	public Wall() {
+		super(true, "sprites/black_background.png");
+		this.setCollisionImage();
+	}
+}
